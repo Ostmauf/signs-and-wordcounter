1370c14afe11e36408c99bd55bbba230f76a22a6
Ostmauf <71626781+Ostmauf@users.noreply.github.com>
2021-08-16 10:14:22 +0300

    Update app.js
    word counter now working.

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const usrInput = document.getElementById("user-input");
 const signCounter = document.getElementById("sign-counter");
+const wordCounter = document.getElementById("word-counter");
 
 window.addEventListener("keyup", () => {
     signCounter.textContent = "Sign counter: " + usrInput.value.length;
-    
+    wordCounter.value = countWords(usrInput.value);
 });
+
+function countWords(str) {
+    str = str.replace(/(^\s*)|(\s*$)/gi,"");
+    str = str.replace(/[ ]{2,}/gi," ");
+    str = str.replace(/\n /,"\n");
+    wordCounter.innerHTML = "word counter: " + str.split(' ').length;
+}
+ 
+ 
+
